bb333749f01a4e67fd35b69df3f2fe99b722a026
Zer0 <114886018+Zer0G0ld@users.noreply.github.com>
2024-11-01 18:26:37 -0300

    Update app.py
    Corrigido problemas com Cogs, Economy e de logs

--- app.py
+++ app.py
@@ -15,9 +15,9 @@ from commands.Economy import Economy
 from commands.Fun import Fun
 from commands.Games import Games
 from commands.Trapaca import Trapaca
-from commands.Moderation import Moderation
-from commands.Moderation import Music
+from commands.Moderation import Moderation, Music
 
+# Carrega variáveis de ambiente
 load_dotenv()
 
 # Configuração do logger
@@ -29,14 +29,15 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
+# Configurações do bot
 TOKEN = os.getenv('DISCORD_TOKEN')
-
 intents = Intents.default()
 intents.messages = True
-description = "Um bot para se divertir e administra"
+description = "Um bot para se divertir e administra o servidor"
 
 bot = commands.Bot(command_prefix="$", intents=intents, description=description)
 
+# Função para exibir o comando de ajuda
 def help_command(message, args, meta):
     help_message = "= Comandos =\n\n"
     
@@ -48,10 +49,22 @@ def help_command(message, args, meta):
 
     return f"```asciidoc\n{help_message}```"
 
+# Comando para carregar um link
 @bot.command(name='link', help='Abre um link fornecido pelo usuário')
 async def link(ctx, url: str):
     await ctx.send(f'Aqui está o link que você solicitou: {url}')
 
+# Função para carregar cogs
+def load_cogs():
+    cogs = [Economy, Fun, Games, Trapaca, Moderation, Music]
+    for cog in cogs:
+        try:
+            bot.add_cog(cog(bot))
+            logger.info(f"Cog {cog.__name__} carregada com sucesso.")
+        except Exception as e:
+            logger.error(f"Error ao carregar a cog {cog.__name__}: {e}")
+
+# Eventos
 @bot.event
 async def on_ready():   
     logger.info(f"Bot está online como {bot.user.name}")
@@ -67,14 +80,14 @@ async def on_ready():
 #                    picture = discord.File(f)
 #                    await channel.send("Olá! Estou online e pronto para interagir.", file=picture)
 
-    bot.add_cog(Economy(bot))
+#    bot.add_cog(Economy(bot))
 
     # Verifica se a cog Economy foi carregada corretamente
-    cog = bot.get_cog('Economy')
-    if cog:
-        logger.info("Cog Economy carregada com sucesso.")
-    else:
-        logger.error("Erro ao carregar a cog Economy.")
+#    cog = bot.get_cog('Economy')
+#    if cog:
+#        logger.info("Cog Economy carregada com sucesso.")
+#    else:
+#        logger.error("Erro ao carregar a cog Economy.")
 
 
 @bot.event
@@ -87,11 +100,11 @@ async def on_message(message):
         data_manager = bot.get_cog('Economy').data_manager
         if not data_manager.user_exists(user_id):
             data_manager.register_new_user(user_id)
-            await message.channel.send(f"Bem-vindo, {message.author.mention}! Uma nova conta foi criada para você com 100 moedas iniciais.")
+            await message.channel.send(f"Bem-vindo, {message.author.mention}! Uma nova conta foi criada para você com 100 moedas iniciais em seu banco.\nPara ve-la digite '$saldo'")
     await bot.process_commands(message)
 
-@commands.Cog.listener()
-async def on_command_error(self, ctx, error):
+@bot.event
+async def on_command_error(ctx, error):
     if isinstance(error, commands.MissingRequiredArgument):
         await ctx.send('Você esqueceu de fornecer um argumento necessário para este comando.')
     elif isinstance(error, commands.CommandNotFound):
@@ -99,13 +112,26 @@ async def on_command_error(self, ctx, error):
     else:
         logger.error(f'Erro ao executar o comando {ctx.command}: {error}')
 
+
+#@commands.Cog.listener()
+#async def on_command_error(self, ctx, error):
+#    if isinstance(error, commands.MissingRequiredArgument):
+#        await ctx.send('Você esqueceu de fornecer um argumento necessário para este comando.')
+#    elif isinstance(error, commands.CommandNotFound):
+#        await ctx.send('Este comando não existe.')
+#    else:
+#        logger.error(f'Erro ao executar o comando {ctx.command}: {error}')
+
 # Carregar extensões
 #bot.add_cog(Economy(bot))
-bot.add_cog(Fun(bot))
-bot.add_cog(Games(bot))
-bot.add_cog(Trapaca(bot))
-bot.add_cog(Moderation(bot))
-bot.add_cog(Music(bot))
+#bot.add_cog(Fun(bot))
+#bot.add_cog(Games(bot))
+#bot.add_cog(Trapaca(bot))
+#bot.add_cog(Moderation(bot))
+#bot.add_cog(Music(bot))
+
+# Chama a função de carregamento de cogs aqui
+load_cogs()
 
 bot.run(TOKEN)
 
